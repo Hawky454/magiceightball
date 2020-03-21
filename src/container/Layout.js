86c68a0831632001dd31266c4cc64638f9f71663
David Miller <davidmiller@Davids-MBP.attlocal.net>
2020-03-21 03:15:54 -0500

    still relearning how to use the css modules but at a stopping point at this poing in time

--- src/container/Layout.js
+++ src/container/Layout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MagicEightBall from '../components/MagicEightBall';
+import styles from './Styles.module.css';
 
 class Layout extends Component{
 
@@ -10,7 +11,7 @@ class Layout extends Component{
 
     render() {
         return (
-            <div>
+            <div className={styles.main}>
              <MagicEightBall />
             </div>
         )
